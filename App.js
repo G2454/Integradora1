fb4d4128f5b68afe12348ff26d7f7331908e2396
Luan Tarumoto de Macedo <Luan.taru@hotmail.com>
2025-01-15 17:24:42 -0300

    Adicionei a Logo, e alguns textos
    ainda falta organizar tudo no lugar certo e deixar os botões funcionais.

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Salve!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <Image
+       source={require("./src/assets/UTFPReventos.png")}  
+       style={styles.logo}    
+      />
+
+      <Text style={styles.title}> Login </Text>
+
+      <View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}> ENTRAR </Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    alignItems: "center",
   },
-});
+  logo: {
+    marginBottom: 400,
+  },
+  title: {
+    fontSize: 24,
+    marginTop: -200,
+  },
+  button: {
+    backgroundColor: "#FFD545",
+    width: 200,
+    height: 55,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 18,
+    marginBottom: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#FFF",
+  }
+})
